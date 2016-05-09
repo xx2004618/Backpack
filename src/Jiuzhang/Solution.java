da933964ae965cce0b6742bd4bbece44836749df
xx2004618 <xx2004618@qq.com>
2016-05-08 23:44:13 -0700

    Java Jiuzhang L5 Backpack

---
+++ src/Jiuzhang/Solution.java
@@ -0,0 +1,19 @@
+package Jiuzhang;
+
+public class Solution {
+    /**
+     * @param m: An integer m denotes the size of a backpack
+     * @param A: Given n items with size A[i]
+     * @return: The maximum size
+     */
+    public int backPack(int m, int[] A) {
+        // write your code here
+    	return 0;
+    }
+    public static void main(String args[]){
+    	Solution solution = new Solution();
+    	int[] m = {2, 3, 5, 7};
+    	int[] A = 10;
+    	System.out.println(solution.backPack(m, A));
+    }
+}
